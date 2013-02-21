87220134db2a740cc91f12595c8878db0bfb1ac8
Martin <mtuspersonal@hotmail.com>
2013-02-20 20:28:04 -0600

    Added the event handler. Needs to be documented.

---
+++ src/mtus/net/reactor/EventHandler.java
@@ -0,0 +1,68 @@
+package mtus.net.reactor;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+
+import mtus.net.reactor.model.Client;
+
+/**
+ * @author Martin Tuskevicius
+ */
+public class EventHandler {
+
+    public void onConnect(SelectionKey selectionKey) throws Exception {
+
+    }
+
+    public void onRead(SelectionKey selectionKey) throws Exception {
+	SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+	Client client = (Client) selectionKey.attachment();
+	ByteBuffer readBuffer = client.getReadBuffer();
+	int amountRead;
+	try {
+	    amountRead = socketChannel.read(readBuffer);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    client.disconnect();
+	    return;
+	}
+	if (amountRead == 0) {
+	    return;
+	} else if (amountRead == -1) {
+	    client.disconnect();
+	    return;
+	}
+	readBuffer.flip();
+	try {
+	    client.handleData();
+	} finally {
+	    if (readBuffer.hasRemaining()) {
+		readBuffer.compact();
+	    } else {
+		readBuffer.clear();
+	    }
+	}
+    }
+
+    public void onWrite(SelectionKey selectionKey) throws Exception {
+	SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+	Client client = (Client) selectionKey.attachment();
+	ByteBuffer writeBuffer = client.getWriteBuffer();
+	writeBuffer.flip();
+	try {
+	    socketChannel.write(writeBuffer);
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    client.disconnect();
+	    return;
+	}
+	if (writeBuffer.hasRemaining()) {
+	    writeBuffer.compact();
+	} else {
+	    writeBuffer.clear();
+	    selectionKey.interestOps(SelectionKey.OP_READ);
+	}
+    }
+}
